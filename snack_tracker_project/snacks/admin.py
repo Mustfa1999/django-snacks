df0ea1431090454200d4f917ede7036950d06d71
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-05-15 13:03:55 +0300

    deleting

--- snack_tracker_project/snacks/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Snack
-
-@admin.register(Snack)
-class AdminSnack(admin.ModelAdmin):
-    list_display = ['id', 'name', 'purchaser', 'description']
-    search_fields = ('name', )
-
-# admin.site.register(Snack)
--- snack_tracker_project/snacks/urls.py
+++
@@ -1,8 +0,0 @@
-import imp
-from django.urls import path
-from .views import SnackListView, SnackDetailView
-
-urlpatterns = [
-    path('snacks-list', SnackListView.as_view(), name='snacks_list'),
-    path('detail-view/<int:pk>', SnackDetailView.as_view(), name='snack_detail'),
-]
